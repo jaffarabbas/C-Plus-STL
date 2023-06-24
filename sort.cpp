9d8823ab2035bad156405b897c52e36bdfdb1361
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-24 20:28:42 +0500

    Commited File : sort.cpp at 24/06/2023 8:28:41 pm

---
+++ sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//hybrid algorithm quick sort intertion sort and quick sort
+int main()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(4);
+	v.push_back(2);
+	v.push_back(6);
+	v.push_back(3);
+
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << endl;
+	}
+	sort(v.begin(),v.end());
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << endl;
+	}
+}
